1f2450ffbf12362652e218185c5c0ad8d772b8e1
Chris Z <ckz8780@gmail.com>
2020-11-30 20:02:42 -0500

    Create 006-rest-parameter-syntax.js

---
+++ 006-rest-parameter-syntax.js
@@ -0,0 +1,10 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node 006-rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+
+// Extra arguments are ignored
+
+// Function using ...rest
